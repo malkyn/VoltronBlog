aa13bd5e535943f26b2ace8ebfe9373facab53bd
vinir <46539805+malkyn@users.noreply.github.com>
2024-05-14 17:03:28 -0300

    Slides das informações de personagens

---
+++ voltron-frontend/src/assets/javascripts/slide.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var cont = 1;
+
+  var radio = document.querySelector('.manual-btn');
+  document.getElementById('radio1').checked = true;
+
+  setInterval(function() {
+    proximaTag();
+  }, 2000);
+
+  function proximaTag() {
+    cont++;
+
+    if (cont > 6) {
+      cont = 1;
+    }
+
+    document.getElementById('radio' + cont).checked = true;
+  }
+});
